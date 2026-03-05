53c1428bd4b8965f98bec0a6baa56c8eaf4ffb2b
Wiyumiyu <mariana.torres.valverde0@gmail.com>
2026-03-05 13:41:28 -0600

    cambiar Program.cs del WEB para usar MVC

--- Vehiculos.WEB/Web/Program.cs
+++ Vehiculos.WEB/Web/Program.cs
@@ -1,25 +1,17 @@
 using Abstracciones.Interfaces.Reglas;
-//using Autorizacion.Abstracciones.DA;
-//using Autorizacion.Abstracciones.Flujo;
-//using Autorizacion.DA;
-//using Autorizacion.DA.Repositorios;
-//using Autorizacion.Flujo;
-//using Autorizacion.Middleware;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Reglas;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
 builder.Services.AddScoped<IConfiguracion, Configuracion>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
@@ -28,6 +20,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.MapRazorPages(); 
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Vehiculos}/{action=Index}/{id?}");
 
 app.Run();
