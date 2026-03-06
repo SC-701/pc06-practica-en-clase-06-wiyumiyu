c146830547f14ceb8b998d6070357a3d67d7ccc0
Wiyumiyu <mariana.torres.valverde0@gmail.com>
2026-03-06 15:21:05 -0600

    corregido

--- Vehiculos.WEB/Web/Program.cs
+++ Vehiculos.WEB/Web/Program.cs
@@ -13,16 +13,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IConfiguracion, Configuracion>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-
-    // The default HSTS value is 30 days. 
-    // You may want to change this for production scenarios.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -33,4 +30,4 @@ app.UseRouting();
 
 app.MapRazorPages();
 
-app.Run(); lder.Services.AddRazorPages(); builder.Services.AddScoped<IConfiguracion, Configuracion>(); var app = builder.Build(); // Configure the HTTP request pipeline. if (!app.Environment.IsDevelopment()) { app.UseExceptionHandler("/Error"); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts. app.UseHsts(); } app.UseHttpsRedirection(); app.UseStaticFiles(); app.UseRouting(); app.MapRazorPages(); app.Run();
+app.Run();
