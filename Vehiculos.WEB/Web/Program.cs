0afaab6470626fce25dba9f79ee97649fe5d647c
Wiyumiyu <mariana.torres.valverde0@gmail.com>
2026-03-06 15:17:53 -0600

    como antes

--- Vehiculos.WEB/Web/Program.cs
+++ Vehiculos.WEB/Web/Program.cs
@@ -1,16 +1,28 @@
 using Abstracciones.Interfaces.Reglas;
+//using Autorizacion.Abstracciones.DA;
+//using Autorizacion.Abstracciones.Flujo;
+//using Autorizacion.DA;
+//using Autorizacion.DA.Repositorios;
+//using Autorizacion.Flujo;
+//using Autorizacion.Middleware;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Reglas;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IConfiguracion, Configuracion>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+
+    // The default HSTS value is 30 days. 
+    // You may want to change this for production scenarios.
     app.UseHsts();
 }
 
@@ -19,8 +31,6 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
-
 app.MapRazorPages();
 
-app.Run();
+app.Run(); lder.Services.AddRazorPages(); builder.Services.AddScoped<IConfiguracion, Configuracion>(); var app = builder.Build(); // Configure the HTTP request pipeline. if (!app.Environment.IsDevelopment()) { app.UseExceptionHandler("/Error"); // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts. app.UseHsts(); } app.UseHttpsRedirection(); app.UseStaticFiles(); app.UseRouting(); app.MapRazorPages(); app.Run();
